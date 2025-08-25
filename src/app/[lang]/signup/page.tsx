fa83140099158ee99a28f49de0d2055090762436
wclimaco2025 <william.climaco@outlook.com>
2025-08-25 10:02:02 -0600

    cambios en la internacionalizacion del formulario de registro

--- src/app/[lang]/signup/page.tsx
+++ src/app/[lang]/signup/page.tsx
@@ -1,10 +1,19 @@
 import { RegisterForm } from "@/app/components";
+import { getDictionary } from "../dictionaries";
 
-const signup =()=>{
+const signup =async ({params}:{params:Promise<{lang:'en-US'|'es-ES'}>})=>{
+    const {lang} = await params;
+    const dict = await getDictionary(lang);
+
     return(
     <>
     <h1>Formulario de Registro</h1>
     <RegisterForm
+     formTitle={dict.FormSignup.formTitle}
+     formUser={dict.FormSignup.formUsername}
+     formPass={dict.FormSignup.formPass}
+     formSubmit={dict.FormSignup.formSubmit}
+
     />
     </>);
 }
